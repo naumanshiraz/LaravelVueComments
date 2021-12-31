c1fdc6dcad6b7419304e5627924f493c48448e25
Nauman Shiraz <nauman_shiraz@hotmail.com>
2021-12-31 14:01:50 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Comment Component For Laravel using VueJS
+
+1. run `composer install` 
+2. run`npm install` 
+3. Copy the existing **.env.example** file and rename it to **.env**. 
+4. Now add your database credentials in it. 
+
+5. run `php artisan migrate`
+
+Once its completed. Open the app in your browser and start using it. 
---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+ <link href="css/comment.css" rel="stylesheet" type="text/css">
+<div class="container" style="background:white;">
+    <div class="row">
+        <div class="col-md-8 col-lg-offset-2" >
+            <comment comment-url=""></comment>
+        </div>      
+    </div>
+</div>
+@endsection
